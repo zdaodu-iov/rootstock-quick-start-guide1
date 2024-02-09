f5d48f70ba5cde8779faf25e95c6ff07e020a404
zdaodu-iov <zainab.daodu@iovlabs.org>
2024-02-09 14:57:35 +0100

    Configure Hardhat for Rootstock

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 // Hardhat configuration
 module.exports = {
   solidity: "0.8.20",
@@ -6,11 +7,13 @@ module.exports = {
       url: "https://public-node.rsk.co",
       chainId: 30,
       gasPrice: 60000000,
+      accounts: [process.env.Rootstock_MAINNET_PRIVATE_KEY]
     },
     rskTestnet: {
       url: "https://public-node.testnet.rsk.co",
       chainId: 31,
       gasPrice: 60000000,
+      accounts: [process.env.Rootstock_TESTNET_PRIVATE_KEY]
     }
   }
 };
